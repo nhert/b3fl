01a7aebfd901b3f893864c49971d7fdc0059755e
Nate H <nathan.hertner@gmail.com>
2023-06-26 17:07:34 -0400

    z

--- tools.js
+++ tools.js
@@ -10,9 +10,9 @@ var SleeperTools = (function () {
 	tools.A_LEAGUE_SLEEPER_ID = "968583157932937216";
 	tools.B_LEAGUE_SLEEPER_ID = "866171664994541568";
 
-	tools.getUsername = function () {
-		// ...
-		// TEST
+	tools.getUsers = async function (leagueId) {
+		await fetch("https://api.sleeper.app/v1/league/" + leagueId)
+                .then((res) => res.json());
 	};
 
 	return tools;
